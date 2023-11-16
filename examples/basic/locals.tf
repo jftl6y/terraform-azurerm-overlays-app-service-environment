252568fb9a89601b2db782926a7d298ec1fd5592
John Scott (Azure) <joscot@microsoft.com>
2023-11-16 15:55:15 -0800

    Correct spelling across repo

--- examples/basic/locals.tf
+++ examples/basic/locals.tf
@@ -6,6 +6,6 @@ locals {
     Project = "Azure NoOps"
     Module  = "overlays-app-service-environment"
     Toolkit = "Terraform"
-    Example = "basic deployment of Azure App Service Environemt"
+    Example = "basic deployment of Azure App Service Environment"
   }
 }
